d1ff684f29f7d0794c8caa529c79e0d6a120d7b8
unknown <akshayarora.10.6.97@gmail.com>
2020-04-02 12:57:10 +0530

    commenting code

--- script.js
+++ script.js
@@ -1,83 +1,68 @@
 const express = require('express'); //Import Express
 const Joi = require('joi'); //Import Joi
 const app = express(); //Create Express Application on the app variable
-const fs = require('fs');
+const fs = require('fs');//Import fs 
 app.use(express.json()); //used the json file
 
-//Give data to the server
-// const allSongs = [
-//     { title: 'George', id: 1 },
-//     { title: 'Josh', id: 2 },
-//     { title: 'Tyler', id: 3 },
-//     { title: 'Alice', id: 4 },
-//     { title: 'Candice', id: 5 }
-// ]
 
-//Read Request Handlers
+//root of API
 // Display the Message when the URL consist of '/'
 app.get('/', (req, res) => {
     res.send("Welcome to Akshay's REST API!");
 });
-// Display the List Of Customers when URL consists of api customers
+// Returns the result of all songs in the DB
 app.get('/api/songs', (req, res) => {
+    //reading DB
     var jsonString = fs.readFileSync('./songs/allSongs.json');
     const parsedJson = JSON.parse(jsonString);
-    console.log(parsedJson.allSongs);
     var allSongs = parsedJson.allSongs;
     for (var i = 0; i < allSongs.length; i++) {
         var allRatings = allSongs[i].ratings;
-        console.log("AllRatings:" + allRatings);
+        //concluding avgRating and adding to object
         delete allSongs[i].ratings;
         allSongs[i].avgRating = getAverageRating(allRatings) || 0;
-        console.log("AvgRating:" + getAverageRating(allRatings));
     }
+    //soting the songs based on avg rating
     var sortedSongs = [...allSongs].sort(sortOnRating);
     function sortOnRating(a, b) {
         if (a.avgRating > b.avgRating) return -1;
         if (a.avgRating < b.avgRating) return 1;
         return 0;
     }
+    //sending the final result
     res.send(sortedSongs);
 });
 
-
 app.get('/api/songsFromArtist', (req, res) => {
     const artist = req.get("artistName");
-    console.log("Artist Name : " + artist);
     const sungSongs = getSungSongs(artist);
     res.send(sungSongs);
 });
 
-
+// Returns the result of all artists in the DB
 app.get('/api/artists', (req, res) => {
+    //Reading artists from DB
     var jsonString = fs.readFileSync('./artists/allArtists.json');
     const parsedJson = JSON.parse(jsonString);
-    console.log(parsedJson.allArtists);
     res.send(parsedJson.allArtists);
 });
-// Display the Information Of Specific Customer when you mention the id.
-// app.get('/api/songs/:id', (req, res) => {
-//     const song = allSongs.find(c => c.id === parseInt(req.params.id));
-//     //If there is no valid customer ID, then display an error with the following message
-//     if (!song) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Ooops... Cant find what you are looking for!</h2>');
-//     res.send(song);
-// });
 
-//CREATE Request Handler
-//CREATE New Customer Information
+
+//CREATE New Song
 app.post('/api/songs', (req, res) => {
 
+    //validating received song object
     const { error } = validateSong(req.body);
     if (error) {
         res.status(400).send(error.details[0].message)
         return;
     }
-    //Increment the customer id
 
-
+    //Reading songs from DB
     var allSongsString = fs.readFileSync('./songs/allSongs.json');
     const parsedJsonSong = JSON.parse(allSongsString);
     const allSongs = parsedJsonSong.allSongs;
+    //Creating new Song object
     const song = {
         id: allSongs.length + 1,
         songName: req.body.songName,
@@ -86,12 +71,15 @@ app.post('/api/songs', (req, res) => {
         ratings: [],
         artists: req.body.artists
     };
+    //Adding new object to all songs
     allSongs.push(song)
 
 
     const finalObjSong = {
         allSongs: allSongs
     }
+
+    //Writing final object back to DB
     const stringToWriteSong = JSON.stringify(finalObjSong, null, 2);
     try {
         fs.writeFileSync('./songs/allSongs.json', stringToWriteSong);
@@ -99,7 +87,8 @@ app.post('/api/songs', (req, res) => {
         console.log(err);
     }
 
-    //now adding song to artist sungSongs
+    //now adding to sungSongs of all artists in the song
+    //Reading artists from DB
     var allArtistsString = fs.readFileSync('./artists/allArtists.json');
     const parsedJsonArtists = JSON.parse(allArtistsString);
     const allArtists = parsedJsonArtists.allArtists;
@@ -116,6 +105,8 @@ app.post('/api/songs', (req, res) => {
     const finalObjArtist = {
         allArtists: allArtists
     }
+
+    //Writing final object to DB
     const stringToWriteArtist = JSON.stringify(finalObjArtist, null, 2);
 
     try {
@@ -126,16 +117,17 @@ app.post('/api/songs', (req, res) => {
     res.send(song);
 });
 
+//Rating a song 
 app.post('/api/songs/rate', (req, res) => {
 
+    //Validating received rating object
     const error = validateRating(req.body.rating);
     if (!error) {
         res.status(400).send("invalid rating");
         return;
     }
-    //Increment the customer id
 
-
+    //Reading songs from DB
     var allSongsString = fs.readFileSync('./songs/allSongs.json');
     const parsedJson = JSON.parse(allSongsString);
 
@@ -143,6 +135,7 @@ app.post('/api/songs/rate', (req, res) => {
     const rating = req.body.rating;
     const songId = req.body.id;
 
+    //Adding Rating
     for (var i = 0; i < allSongs.length; i++) {
         if (allSongs[i].id == songId) {
             allSongs[i].ratings.push(rating);
@@ -153,6 +146,8 @@ app.post('/api/songs/rate', (req, res) => {
     const finalObj = {
         allSongs: allSongs
     }
+
+    //Writing final object to DB
     const stringToWrite = JSON.stringify(finalObj, null, 2);
     try {
         fs.writeFileSync('./songs/allSongs.json', stringToWrite);
@@ -162,20 +157,23 @@ app.post('/api/songs/rate', (req, res) => {
     res.send({ id: songId, rating });
 });
 
+//CREATE New Artist
 app.post('/api/artists', (req, res) => {
 
+    //Validating received artist object
     const { error } = validateArtist(req.body);
     if (error) {
         res.status(400).send(error.details[0].message)
         return;
     }
-    //Increment the customer id
 
-
+    //Reading all artists from DB
     var allArtistsString = fs.readFileSync('./artists/allArtists.json');
     const parsedJson = JSON.parse(allArtistsString);
 
     const allArtists = parsedJson.allArtists;
+
+    //Creating new Artist object
     const artist = {
         id: allArtists.length + 1,
         artistName: req.body.artistName,
@@ -183,11 +181,15 @@ app.post('/api/artists', (req, res) => {
         bio: req.body.bio,
         sungSongs:[]
     };
+
+    //Pushing object to all artists
     allArtists.push(artist)
 
     const finalObj = {
         allArtists: allArtists
     }
+
+    //Writing final object to DB
     const stringToWrite = JSON.stringify(finalObj, null, 2);
     try {
         fs.writeFileSync('./artists/allArtists.json', stringToWrite);
@@ -197,27 +199,15 @@ app.post('/api/artists', (req, res) => {
     res.send(artist);
 });
 
-//Update Request Handler
-// Update Existing Customer Information
-// app.put('/api/songs/:id', (req, res) => {
-//     const song = allSongs.find(c => c.id === parseInt(req.params.id));
-//     if (!song) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Not Found!! </h2>');
-
-//     const { error } = validateSong(req.body);
-//     if (error) {
-//         res.status(400).send(error.details[0].message);
-//         return;
-//     }
-
-//     song.title = req.body.title;
-//     res.send(song);
-// });
 
 //Delete Request Handler
-// Delete Customer Details
+
+//DELETE a song
 app.delete('/api/songs/:id', (req, res) => {
 
     const id = req.params.id;
+
+    //Reading all songs from DB
     var allSongsString = fs.readFileSync('./songs/allSongs.json');
     const parsedJson = JSON.parse(allSongsString);
 
@@ -227,12 +217,15 @@ app.delete('/api/songs/:id', (req, res) => {
 
     for (var i = 0; i < allSongs.length; i++) {
         if (allSongs[i].id == id) {
+            //deleting this song based on id
             deletedIndex = i;
             deletedElement = allSongs[i];
             allSongs.splice(i, 1);
             break;
         }
     }
+
+    //Re-assigning new IDs to remaining songs
     if (deletedIndex) {
         for (var i = deletedIndex; i < allSongs.length; i++) {
             allSongs[i].id = allSongs[i].id - 1;
@@ -242,6 +235,8 @@ app.delete('/api/songs/:id', (req, res) => {
     const finalObj = {
         allSongs: allSongs
     }
+
+    //Writing final object to DB
     const stringToWrite = JSON.stringify(finalObj, null, 2);
     try {
         fs.writeFileSync('./songs/allSongs.json', stringToWrite);
@@ -251,9 +246,13 @@ app.delete('/api/songs/:id', (req, res) => {
     res.send(deletedElement || "Invalid ID");
 });
 
+
+//DELETE an Artist
 app.delete('/api/artists/:id', (req, res) => {
 
     const id = req.params.id;
+
+    //Reading all artists from DB
     var allArtistString = fs.readFileSync('./artists/allArtists.json');
     const parsedJson = JSON.parse(allArtistString);
 
@@ -263,6 +262,7 @@ app.delete('/api/artists/:id', (req, res) => {
 
     for (var i = 0; i < allArtists.length; i++) {
         if (allArtists[i].id == id) {
+            //deleting this artists based on artist
             deletedIndex = i;
             deletedElement = allArtists[i];
             allArtists.splice(i, 1);
@@ -270,6 +270,7 @@ app.delete('/api/artists/:id', (req, res) => {
         }
     }
 
+    //Re-assigning new IDs to remaining artists
     if (deletedIndex) {
         for (var i = deletedIndex; i < allArtists.length; i++) {
             allArtists[i].id = allArtists[i].id - 1;
@@ -279,6 +280,8 @@ app.delete('/api/artists/:id', (req, res) => {
     const finalObj = {
         allArtists: allArtists
     }
+
+    //Writing final object to DB
     const stringToWrite = JSON.stringify(finalObj, null, 2);
     try {
         fs.writeFileSync('./artists/allArtists.json', stringToWrite);
@@ -290,6 +293,8 @@ app.delete('/api/artists/:id', (req, res) => {
 
 
 //Validate Information
+
+//function to validate song
 function validateSong(song) {
     const schema = {
         songName: Joi.string().min(3).required(),
@@ -298,9 +303,9 @@ function validateSong(song) {
         artists: Joi.array().required().min(1)
     };
     return Joi.validate(song, schema);
-
 }
 
+//function to validate rating
 function validateRating(rating) {
 
     if (rating < 0 || rating > 5) {
@@ -309,6 +314,7 @@ function validateRating(rating) {
     return true;
 }
 
+//function to validaet artist
 function validateArtist(artist) {
     const schema = {
         artistName: Joi.string().min(3).required(),
@@ -319,6 +325,7 @@ function validateArtist(artist) {
 
 }
 
+//get all sung songs from a particular artist
 function getSungSongs(artist) {
     console.log("ArtistName : " + artist);
     var sungSongs = []
@@ -336,6 +343,7 @@ function getSungSongs(artist) {
     return sungSongs;
 }
 
+//get average rating from all ratings
 function getAverageRating(ratings) {
     var avgRating = 0;
     for (var i = 0; i < ratings.length; i++) {
